aa96289f93f403fd5bfc29f8601b40a85dbaeb76
Mark Faldborg <mark@faldb.org>
2013-03-12 10:57:06 +0100

    Moved the pictocreator specific code into source/PictoCreator

---
+++ source/PictoCreator/gen/R.java.d
@@ -0,0 +1,16 @@
+/home/mark/git/SW6/pictocreator/source/gen/dk/aau/cs/giraf/pictocreator/R.java \
+ : /home/mark/git/SW6/pictocreator/source/res/layout/activity_main.xml \
+/home/mark/git/SW6/pictocreator/source/res/values/styles.xml \
+/home/mark/git/SW6/pictocreator/source/res/values/strings.xml \
+/home/mark/git/SW6/pictocreator/source/res/drawable-mdpi/ic_launcher.png \
+/home/mark/git/SW6/pictocreator/source/res/drawable-xhdpi/ic_launcher.png \
+/home/mark/git/SW6/pictocreator/source/res/values-v14/styles.xml \
+/home/mark/git/SW6/pictocreator/source/res/drawable-ldpi/ic_launcher.png \
+/home/mark/git/SW6/pictocreator/source/res/values-v11/styles.xml \
+/home/mark/git/SW6/pictocreator/source/res/drawable-hdpi/ic_launcher.png \
+/home/mark/git/SW6/pictocreator/source/res/menu/activity_main.xml \
+/home/mark/git/SW6/pictocreator/source/bin/res/drawable-mdpi/ic_launcher.png \
+/home/mark/git/SW6/pictocreator/source/bin/res/drawable-xhdpi/ic_launcher.png \
+/home/mark/git/SW6/pictocreator/source/bin/res/drawable-ldpi/ic_launcher.png \
+/home/mark/git/SW6/pictocreator/source/bin/res/drawable-hdpi/ic_launcher.png \
+/home/mark/git/SW6/pictocreator/source/bin/AndroidManifest.xml \
