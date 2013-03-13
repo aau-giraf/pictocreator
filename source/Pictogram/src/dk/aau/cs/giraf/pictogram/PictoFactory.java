b0d77361b2225ba1757bd3c42da6c814a1dbe6ea
Mark Faldborg <mark@faldb.org>
2013-03-13 13:05:01 +0100

    Changes that were made were changed to make room for changes.

--- source/Pictogram/src/dk/aau/cs/giraf/pictogram/PictoFactory.java
+++ source/Pictogram/src/dk/aau/cs/giraf/pictogram/PictoFactory.java
@@ -4,95 +4,96 @@ import java.util.ArrayList;
 
 public enum PictoFactory {
     INSTANCE;
-    private ArrayList<String> temp_image_database;
-    private ArrayList<String> temp_audio_database;
-    private ArrayList<String> temp_text_database;
+    private ArrayList<String> tempImageDatabase;
+    private ArrayList<String> tempAudioDatabase;
+    private ArrayList<String> tempTextDatabase;
 
-    private PictoFactory(){
-        String _temp_image_database[] = { "/sdcard/Pictogram/bade.wma",
-                                          "/sdcard/Pictogram/børste_tænder.wma",
-                                          "/sdcard/Pictogram/drikke.wma",
-                                          "/sdcard/Pictogram/du.wma",
-                                          "/sdcard/Pictogram/film.wma",
-                                          "/sdcard/Pictogram/for_højt.wma",
-                                          "/sdcard/Pictogram/gå.wma",
-                                          "/sdcard/Pictogram/ja.wma",
-                                          "/sdcard/Pictogram/køre.wma",
-                                          "/sdcard/Pictogram/lave_mad.wma",
-                                          "/sdcard/Pictogram/lege.wma",
-                                          "/sdcard/Pictogram/mig.wma",
-                                          "/sdcard/Pictogram/morgen_routine.wma",
-                                          "/sdcard/Pictogram/nej.wma",
-                                          "/sdcard/Pictogram/se.wma",
-                                          "/sdcard/Pictogram/side_ned.wma",
-                                          "/sdcard/Pictogram/spille_computer.wma",
-                                          "/sdcard/Pictogram/stop.wma",
-                                          "/sdcard/Pictogram/sulten.wma",
-                                          "/sdcard/Pictogram/søvnig.wma",
-                                          "/sdcard/Pictogram/tale_sammen.wma",
-                                          "/sdcard/Pictogram/tørstig.wma",
-                                          "/sdcard/Pictogram/være_stille.wma"};
+    public void repopulateTemporaryDatabase(){
+        String _tempImageDatabase[] = { "/sdcard/Pictogram/bade.wma",
+                                        "/sdcard/Pictogram/børstetænder.wma",
+                                        "/sdcard/Pictogram/drikke.wma",
+                                        "/sdcard/Pictogram/du.wma",
+                                        "/sdcard/Pictogram/film.wma",
+                                        "/sdcard/Pictogram/forhøjt.wma",
+                                        "/sdcard/Pictogram/gå.wma",
+                                        "/sdcard/Pictogram/ja.wma",
+                                        "/sdcard/Pictogram/køre.wma",
+                                        "/sdcard/Pictogram/lavemad.wma",
+                                        "/sdcard/Pictogram/lege.wma",
+                                        "/sdcard/Pictogram/mig.wma",
+                                        "/sdcard/Pictogram/morgenroutine.wma",
+                                        "/sdcard/Pictogram/nej.wma",
+                                        "/sdcard/Pictogram/se.wma",
+                                        "/sdcard/Pictogram/sidened.wma",
+                                        "/sdcard/Pictogram/spillecomputer.wma",
+                                        "/sdcard/Pictogram/stop.wma",
+                                        "/sdcard/Pictogram/sulten.wma",
+                                        "/sdcard/Pictogram/søvnig.wma",
+                                        "/sdcard/Pictogram/talesammen.wma",
+                                        "/sdcard/Pictogram/tørstig.wma",
+                                        "/sdcard/Pictogram/værestille.wma"};
 
-        String _temp_audio_database[] = {"/sdcard/Pictogram/Bade.png",
-                                         "/sdcard/Pictogram/Børste_Tænder.png",
-                                         "/sdcard/Pictogram/Drikke.png",
-                                         "/sdcard/Pictogram/Du.png",
-                                         "/sdcard/Pictogram/Film.png",
-                                         "/sdcard/Pictogram/For_Højt.png",
-                                         "/sdcard/Pictogram/Gå.png",
-                                         "/sdcard/Pictogram/Ja.png",
-                                         "/sdcard/Pictogram/Køre.png",
-                                         "/sdcard/Pictogram/Lave_Mad.png",
-                                         "/sdcard/Pictogram/Lege.png",
-                                         "/sdcard/Pictogram/Mig.png",
-                                         "/sdcard/Pictogram/Morgen_Routine.png",
-                                         "/sdcard/Pictogram/Nej.png",
-                                         "/sdcard/Pictogram/Se.png",
-                                         "/sdcard/Pictogram/Side_Ned.png",
-                                         "/sdcard/Pictogram/Spille_Computer.png",
-                                         "/sdcard/Pictogram/Stop.png",
-                                         "/sdcard/Pictogram/Sulten.png",
-                                         "/sdcard/Pictogram/Søvnig.png",
-                                         "/sdcard/Pictogram/Tale_Sammen.png",
-                                         "/sdcard/Pictogram/Tørstig.png",
-                                         "/sdcard/Pictogram/Være_Stille.png"};
+        String _tempAudioDatabase[] = {"/sdcard/Pictogram/Bade.png",
+                                       "/sdcard/Pictogram/BørsteTænder.png",
+                                       "/sdcard/Pictogram/Drikke.png",
+                                       "/sdcard/Pictogram/Du.png",
+                                       "/sdcard/Pictogram/Film.png",
+                                       "/sdcard/Pictogram/ForHøjt.png",
+                                       "/sdcard/Pictogram/Gå.png",
+                                       "/sdcard/Pictogram/Ja.png",
+                                       "/sdcard/Pictogram/Køre.png",
+                                       "/sdcard/Pictogram/LaveMad.png",
+                                       "/sdcard/Pictogram/Lege.png",
+                                       "/sdcard/Pictogram/Mig.png",
+                                       "/sdcard/Pictogram/MorgenRoutine.png",
+                                       "/sdcard/Pictogram/Nej.png",
+                                       "/sdcard/Pictogram/Se.png",
+                                       "/sdcard/Pictogram/SideNed.png",
+                                       "/sdcard/Pictogram/SpilleComputer.png",
+                                       "/sdcard/Pictogram/Stop.png",
+                                       "/sdcard/Pictogram/Sulten.png",
+                                       "/sdcard/Pictogram/Søvnig.png",
+                                       "/sdcard/Pictogram/TaleSammen.png",
+                                       "/sdcard/Pictogram/Tørstig.png",
+                                       "/sdcard/Pictogram/VæreStille.png"};
 
-        String _temp_text_database[] = {"Bade",
-                                        "Børste Tænder",
-                                        "Drikke",
-                                        "Du",
-                                        "Film",
-                                        "For Højt",
-                                        "Gå",
-                                        "Ja",
-                                        "Køre",
-                                        "Lave Mad",
-                                        "Lege",
-                                        "Mig",
-                                        "Morgen Routine",
-                                        "Nej",
-                                        "Se",
-                                        "Side Ned",
-                                        "Spille Computer",
-                                        "Stop",
-                                        "Sulten",
-                                        "Søvnig",
-                                        "Tale Sammen",
-                                        "Tørstig",
-                                        "Være Stille"};
+        String _tempTextDatabase[] = {"Bade",
+                                      "Børste Tænder",
+                                      "Drikke",
+                                      "Du",
+                                      "Film",
+                                      "For Højt",
+                                      "Gå",
+                                      "Ja",
+                                      "Køre",
+                                      "Lave Mad",
+                                      "Lege",
+                                      "Mig",
+                                      "Morgen Routine",
+                                      "Nej",
+                                      "Se",
+                                      "Side Ned",
+                                      "Spille Computer",
+                                      "Stop",
+                                      "Sulten",
+                                      "Søvnig",
+                                      "Tale Sammen",
+                                      "Tørstig",
+                                      "Være Stille"};
 
-        int length = _temp_image_database.length;
-        
+        // honestly I didn't check but I expect that they are exactly the same length.
+        int length = _tempImageDatabase.length;
+
         for(int i = 0; i < length; i++){
-            temp_image_database.add(_temp_image_database[i]);
+            tempImageDatabase.add(_tempImageDatabase[i]);
         }
 
         for(int i = 0; i < length; i++){
-            temp_text_database.add(_temp_text_database[i]);
+            tempTextDatabase.add(_tempTextDatabase[i]);
         }
 
         for(int i = 0; i < length; i++){
-            temp_audio_database.add(_temp_audio_database[i]);
+            tempAudioDatabase.add(_tempAudioDatabase[i]);
         }
     }
 
@@ -108,7 +109,18 @@ public enum PictoFactory {
         // collections of these things just by the switch
         // of a method.
 
+        if(tempImageDatabase.isEmpty()){
+            // completely arbirary, but hey!
+            repopulateTemporaryDatabase();
+        }
 
+        String pic = tempImageDatabase.get(0);
+        String aud = tempAudioDatabase.get(0);
+        String text = tempTextDatabase.get(0);
+
+        tempImageDatabase.remove(0);
+        tempAudioDatabase.remove(0);
+        tempTextDatabase.remove(0);
 
         //TODO replace this when a new snappier version of
         // Pictogram gets implemented.
@@ -116,5 +128,4 @@ public enum PictoFactory {
 
         return pictogram;
     }
-
 }
