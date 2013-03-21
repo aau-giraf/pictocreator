4ff4b606247da0f620b68cfffbbb4e2a039233eb
Mark Faldborg <mark@faldb.org>
2013-03-21 12:08:40 +0100

    Added more reliable way of adding pictograms temporarily.

--- source/Pictogram/src/dk/aau/cs/giraf/pictogram/PictoFactory.java
+++ source/Pictogram/src/dk/aau/cs/giraf/pictogram/PictoFactory.java
@@ -9,55 +9,82 @@ import android.os.Environment;
 //TODO: If made local, set it to run in seperate thread (DBsync and traversing can be costly)
 public enum PictoFactory {
     INSTANCE;
+    privat final static TAG = "PictoFactory";
 
     private ArrayList<String> tempImageDatabase = new ArrayList<String>();
     private ArrayList<String> tempAudioDatabase = new ArrayList<String>();
     private ArrayList<String> tempTextDatabase = new ArrayList<String>();
 
     public void repopulateTemporaryDatabase(){
-        String _tempAudioDatabase[] = {"/bade.wma",
-                                       "/drikke.wma",
-                                       "/du.wma",
-                                       "/film.wma",
-                                       "/ja.wma",
-                                       "/lege.wma",
-                                       "/mig.wma",
-                                       "/nej.wma",
-                                       "/se.wma",
-                                       "/stop.wma",
-                                       "/sulten.wma"};
+        // String _tempAudioDatabase[] = {"/bade.wma",
+        //                                "/drikke.wma",
+        //                                "/du.wma",
+        //                                "/film.wma",
+        //                                "/ja.wma",
+        //                                "/lege.wma",
+        //                                "/mig.wma",
+        //                                "/nej.wma",
+        //                                "/se.wma",
+        //                                "/stop.wma",
+        //                                "/sulten.wma"};
 
-        String _tempImageDatabase[] = {"/Bade.png",
-                                       "/Drikke.png",
-                                       "/Du.png",
-                                       "/Film.png",
-                                       "/Ja.png",
-                                       "/Lege.png",
-                                       "/Mig.png",
-                                       "/Nej.png",
-                                       "/Se.png",
-                                       "/Stop.png",
-                                       "/Sulten.png"};
+        // String _tempImageDatabase[] = {"/Bade.png",
+        //                                "/Drikke.png",
+        //                                "/Du.png",
+        //                                "/Film.png",
+        //                                "/Ja.png",
+        //                                "/Lege.png",
+        //                                "/Mig.png",
+        //                                "/Nej.png",
+        //                                "/Se.png",
+        //                                "/Stop.png",
+        //                                "/Sulten.png"};
 
-        String _tempTextDatabase[] = {"Bade",
-                                      "Drikke",
-                                      "Du",
-                                      "Film",
-                                      "Ja",
-                                      "Lege",
-                                      "Mig",
-                                      "Nej",
-                                      "Se",
-                                      "Stop",
-                                      "Sulten"};
+        // String _tempTextDatabase[] = {"Bade",
+        //                               "Drikke",
+        //                               "Du",
+        //                               "Film",
+        //                               "Ja",
+        //                               "Lege",
+        //                               "Mig",
+        //                               "Nej",
+        //                               "Se",
+        //                               "Stop",
+        //                               "Sulten"};
+
+        String _tempImageDatabase[];
+        String _tempAudioDatabase[];
+        String _tempTextDatabase[];
+
+        String storagePath = Environment.getExternalStorageDirectory().getPath()
+            + "/Pictures/giraf/public";
+        File storage = new File(storagePath);
+        File[] pictogramStorage = storage.listFiles();
+
+        for(File f : pictogramStorage){
+            String path = f.getPath();
+            int lastIndex = path.length() - 1;
+            String ext = path.substring(lastIndex-4, lastIndex);
+            try{
+                if(ext.equals(".png")){
+                    tempImageDatabase.add(path);
+                    tempTextDatabase.add(f.getName());
+                } else if(ext.equals(".wma")){
+                    tempAudioDatabase.add(path);
+                }
+            } catch(IndexOutOfBoundsException exception){
+                Log.i(TAG, "File name too short to consider a media file.");
+            }
+        }
+
+        _tempImageDatabase = (String[]) tempImageDatabase.toArray();
+        _tempTextDatabase = (String[]) tempTextDatabase.toArray();
+        _tempAudioDatabase = (String[]) tempAudioDatabase.toArray();
 
         // honestly I didn't check but I expect that they are exactly the same length.
         int length = _tempImageDatabase.length;
-        String storagePath = Environment.getExternalStorageDirectory().getPath()
-            + "/Pictures/giraf/public";
 
         for(int i = 0; i < length; i++){
-
             tempImageDatabase.add(storagePath + _tempImageDatabase[i]);
             tempTextDatabase.add(_tempTextDatabase[i]);
             tempAudioDatabase.add(storagePath + _tempAudioDatabase[i]);
