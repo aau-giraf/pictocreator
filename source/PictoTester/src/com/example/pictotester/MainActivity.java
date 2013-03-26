56d9e41d96c91704b5d626ae4083f8fd9d481b6d
Mark Faldborg <mark@faldb.org>
2013-03-26 14:48:32 +0100

    Made tester a little testier.

--- source/PictoTester/src/com/example/pictotester/MainActivity.java
+++ source/PictoTester/src/com/example/pictotester/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pictotester;
 import java.io.IOException;
+import java.util.List;
 
 import android.media.MediaPlayer;
 import dk.aau.cs.giraf.pictogram.*;
@@ -16,42 +17,55 @@ import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
-	@SuppressLint("NewApi")
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT , LayoutParams.MATCH_PARENT);
-		LayoutParams params2 = new LayoutParams(100 , 100);
-		
-		LinearLayout grid = new LinearLayout(this);
-		addContentView(grid, params);
-		
-		Pictogram[] arr = new Pictogram[10];
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		for (int i = 0; i < 10; i++) {
-			arr[i] = PictoFactory.INSTANCE.getPictogram(this, i);
-			arr[i].renderImage();
-			arr[i].renderText();
-			grid.addView(arr[i], params2);
-		}
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT , LayoutParams.MATCH_PARENT);
+        LayoutParams params2 = new LayoutParams(100 , 100);
 
-		//arr[0].playAudio();
-		
-		/*
-		Pictogram pic = PictoFactory.INSTANCE.getPictogram(this, 0);
-		pic.renderImage();
-		pic.renderText();
-		grid.addView(pic);
-		*/
-	}
+        LinearLayout grid = new LinearLayout(this);
+        addContentView(grid, params);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        Pictogram[] arr = new Pictogram[10];
+        List<Pictogram> pictograms = PictoFactory.INSTANCE.getAllPictograms(this);
+
+        for(int i = 0; i < 10; i++){
+            Pictogram pictogram = pictograms.get(i);
+            pictogram.renderImage();
+            pictogram.renderText();
+
+            grid.addView(pictogram, params2);
+        }
+        for(Pictogram p : pictograms){
+        	if(p.hasAudio()){
+        		p.playAudio();
+        		break;
+        	}
+        }
+        // for (int i = 1; i < 10; i++) {
+        //         arr[i].renderImage();
+        //         arr[i].renderText();
+        //         grid.addView(arr[i], params2);
+        // }
+
+        //arr[0].playAudio();
+
+        /*
+          Pictogram pic = PictoFactory.INSTANCE.getPictogram(this, 0);
+          pic.renderImage();
+          pic.renderText();
+          grid.addView(pic);
+        */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
 }
