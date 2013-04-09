d436ec8b7d8d4e6cd0dad9caabe7088ca898433a
René K. Hornbjerg <r.hornbjerg@gmail.com>
2013-04-09 20:49:27 +0200

    R file updated

--- source/AudioRecorder/gen/dk/aau/cs/giraf/audiorecorder/R.java
+++ source/AudioRecorder/gen/dk/aau/cs/giraf/audiorecorder/R.java
@@ -24,7 +24,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int decibel_meter=0x7f080001;
+        public static final int record_button=0x7f080000;
     }
     public static final class layout {
         public static final int activity_record=0x7f030000;
