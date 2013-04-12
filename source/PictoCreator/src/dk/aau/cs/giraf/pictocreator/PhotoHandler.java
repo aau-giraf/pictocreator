d9e690879e9cfc00f2dfa916c94d3e75ceca999f
nattfari <nattfari@fenrir.(none)>
2013-04-12 15:10:18 +0200

    GUI work started on crocactivity, first draft of fragment under construction

---
+++ source/PictoCreator/src/dk/aau/cs/giraf/pictocreator/PhotoHandler.java
@@ -0,0 +1,72 @@
+package dk.aau.cs.giraf.pictocreator;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+//Vogella's photohandler
+
+public class PhotoHandler implements PictureCallback {
+	private final static String TAG = "PhotoHandler";
+
+	private final Context context;
+	
+	public PhotoHandler(Context context) {
+		this.context = context;
+	}
+	
+	@Override
+	public void onPictureTaken(byte[] data, Camera camera) {
+		
+		File pictureFileDir = getDir();
+		
+		if(!pictureFileDir.exists() && !pictureFileDir.mkdirs()) {
+			Log.d(TAG, "Cannot create directory for the image");
+			return;
+		}
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HHmmss");
+		String date = dateFormat.format(new Date());
+		String photoFile = "GImage_" + date + ".jpg";
+		
+		String fileName = pictureFileDir.getPath() + File.separator + photoFile;
+		
+		File pictureFile = new File(fileName);
+		
+		try {
+			FileOutputStream fos = new FileOutputStream(pictureFile);
+			fos.write(data);
+			fos.close();
+			Toast.makeText(context, "Giraf image: " + photoFile + "\n" + 
+									"Saved in: " + pictureFileDir, Toast.LENGTH_LONG).show();
+		} catch(Exception e) {
+			Log.d(TAG, "Picture: " + photoFile + " was not saved" + e.getMessage());
+			Toast.makeText(context, "Image could not be saved", Toast.LENGTH_LONG).show();
+		}
+		
+	}
+	
+	private File getDir() {
+		File storageDir;
+		if(hasExternalStorage()) {
+			storageDir = Environment.getExternalStorageDirectory();
+		}
+		else {
+			storageDir = Environment.getRootDirectory();
+		}
+		return new File(storageDir, "GirafPictures");
+	}
+	
+	private boolean hasExternalStorage() {
+		return (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED));
+	}
+
+}
