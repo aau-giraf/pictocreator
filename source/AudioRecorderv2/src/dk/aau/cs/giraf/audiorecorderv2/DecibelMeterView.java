332672b4367685ce29ca38fb172516331ee5a86a
René K. Hornbjerg <r.hornbjerg@gmail.com>
2013-04-17 16:04:54 +0200

    Version two of the audiorecorder

---
+++ source/AudioRecorderv2/src/dk/aau/cs/giraf/audiorecorderv2/DecibelMeterView.java
@@ -0,0 +1,108 @@
+package dk.aau.cs.giraf.audiorecorderv2;
+
+import android.view.View;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.util.AttributeSet;
+
+
+/**
+ * Class for creation of the view reprecenting the decibel meter
+ * @author Croc
+ *
+ */
+public class DecibelMeterView extends View {
+
+    private double _level = -1;
+
+    private final int[] meterColors = {0xff5555ff,
+                                       0xff5555ff,
+                                       0xff00ff00,
+                                       0xff00ff00,
+                                       0xff00ff00,
+                                       0xff00ff00,
+                                       0xffffff00,
+                                       0xffffff00,
+                                       0xffff0000,
+                                       0xffff0000};
+
+    private final int offColor = 0xff555555;
+
+    private ShapeDrawable drawable;
+
+    /**
+     * Constructor for the view
+     * @param context The context which the view is created in
+     */
+    public DecibelMeterView(Context context){
+        super(context);
+    }
+
+    /**
+     * Constructor for the view
+     * @param context The context which the view is created in
+     * @param attrs The attributes for the view, defined by XML
+     */
+    public DecibelMeterView(Context context, AttributeSet attrs){
+        super(context, attrs);
+    }
+
+    /**
+     * Get function for the decibelmeter level
+     * @return The current level for the decibelmeter
+     */
+    public double getLevel(){
+        return _level;
+    }
+
+    /**
+     * Set function for the decibelmeter level
+     * @param level The level to set in the decibelmeter
+     */
+    public void setLevel(double level){
+        // TODO implement set level function
+        _level = level;
+        invalidate();
+    }
+
+    /**
+     * Function for drawing the decibelmeter
+     * @param canvas The canvas to draw the meter on
+     */
+    private void drawMeter(Canvas canvas){
+        int meterSize = meterColors.length;
+
+        int padding = 5;
+        int x = 10;
+        int y = 0;
+
+        int width = 50;
+
+        int height = (int) (Math.floor(getHeight() / meterSize)) - (2 * padding);
+
+        drawable = new ShapeDrawable(new RectShape());
+
+        for(int i = meterSize - 1; i >= 0 ; i--){
+            y = y + padding;
+            if(((_level - 0.1) * meterSize) >  (i - 0.5)){
+                drawable.getPaint().setColor(meterColors[i]);
+            }
+            else {
+                drawable.getPaint().setColor(offColor);
+            }
+            drawable.setBounds(x, y, x + width, y + height);
+            drawable.draw(canvas);
+            y = y + height + padding;
+        }
+    }
+
+    /**
+     * Draw function, called by android when the view is changed
+     */
+    @Override
+    protected void onDraw(Canvas canvas){
+        drawMeter(canvas);
+    }
+}
---
+++ source/AudioRecorderv2/src/dk/aau/cs/giraf/audiorecorderv2/RecordMainActivity.java
@@ -0,0 +1,113 @@
+package dk.aau.cs.giraf.audiorecorderv2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ToggleButton;
+/**
+ * Main activity for the audio recorder
+ * @author Croc
+ *
+ */
+public class RecordMainActivity extends Activity // implements RecordInterface
+{
+
+    private static final String TAG = "RecordMainActivity";
+
+    AudioHandler handler;
+
+    DecibelMeterView decibelMeter;
+
+    RecordThread recThread;
+
+    ToggleButton recordButton;
+
+    /**
+     * Called when the activity is first created
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_main);
+
+    //     handler = new AudioHandler();
+
+    //     recThread = new RecordThread(handler, this);
+
+    //     recordButton = (ToggleButton) findViewById(R.id.record_button);
+
+    //     decibelMeter = (DecibelMeterView) findViewById(R.id.decibel_meter);
+
+
+    //     OnClickListener clickListener = new OnClickListener() {
+    //             public void onClick(View view) {
+    //                 if (((ToggleButton) view).isChecked()) {
+    //                     recThread.start();
+    //                 }
+    //                 else {
+    //                     recThread.stop();
+    //                 }
+    //             }
+    //         };
+    // recordButton.setOnClickListener(clickListener);
+
+        RecordDialogFragment recordDialog = new RecordDialogFragment();
+        recordDialog.show(getFragmentManager(), TAG);
+    }
+
+    /**
+     * Creates the action bar
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.record_main, menu);
+        return true;
+    }
+
+    /**
+     * Override function for the RecordInterface
+     * @param decibelValue The value to set in the decibelmeter
+     */
+    // @Override
+    // public void decibelUpdate(double decibelValue){
+
+    //     final double dbValue = decibelValue;
+
+    //     decibelMeter.post(new Runnable() {
+    //             /**
+    //              * Function to run when the Thread is started
+    //              */
+    //             @Override
+    //             public void run(){
+    //                 decibelMeter.setLevel(dbValue);
+    //             }
+    //         });
+    // }
+
+    // /**
+    //  * Override function
+    //  * Called when the activity is paused
+    //  */
+    // @Override
+    // protected void onPause(){
+    //     super.onPause();
+    //     recThread.mediaRecorder.release();
+    // }
+
+    //         decibelMeter.post(new Runnable() {
+
+    //                 @Override
+    //                 public void run() {
+
+    //                     decibelMeter.setLevel((offSetDB + rmsDB) / 70);
+    //                     System.out.println(TAG + "DB Level: " + ((offSetDB + rmsDB) / 70));
+    //                     isDrawing = false;
+    //                 }
+    //             });
+    //     }
+
+
+}
