37f6b7aa888510250b483831acd60840ff2a1fd4
Mark Faldborg <mark@faldb.org>
2013-04-25 16:12:34 +0200

    Deleted some trash

--- source/PictoCreator/src/dk/aau/cs/giraf/pictocreator/StoragePictogram.java
+++ source/PictoCreator/src/dk/aau/cs/giraf/pictocreator/StoragePictogram.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
+import android.util.Log;
 
 /**
  * @author: croc
@@ -20,8 +21,9 @@ public class StoragePictogram {
     private String audioPath;
     private long author;
     private boolean publicPictogram;
-    private ArrayList<String> tags;
-    private ArrayList<Tag> allTags;
+    private List<String> tags; // tags added by the user which should be converted via generateTagList
+    private List<Tag> tagList;
+    private List<Tag> globalTags;
     private long pictogramID;
     private Context context;
     private Helper databaseHelper;
@@ -41,41 +43,58 @@ public class StoragePictogram {
     // public setImagePath(String imagePath);
     // public setTextLabel(String setTextLabel);
     // public setAudioPath(String audioPath);
-
-    private List<Tag> generateTagList(){
-        ArrayList<Tag> addedTags = new ArrayList<Tag>();
+    private Tag insertTag(String tag){
+        Tag newTag = null;
         TagsHelper tagsHelper = databaseHelper.tagsHelper;
-        Boolean added;
-        if(!tags.isEmpty()){
-            if(!allTags.isEmpty())
-                for(String tag : tags){
-                    added = false;
-                    for(Tag t : allTags){
-                        if(t.getCaption().equals(tag)){
-                            addedTags.add(t);
-                            added = true;
-                            break;
-                        }
-                    }
+        boolean added = false;
 
-
-                if(!added){
-                    Tag newTag = new Tag(tag);
-                    long tagId;
-                    tagId = tagsHelper.insertTag(newTag);
-                    newTag.setId(tagId);
-
-                    addedTags.add(newTag);
+        if(!globalTags.isEmpty()){
+            for(Tag t : globalTags){
+                if(t.getCaption().equals(tag)){
+                    newTag = t;
+                    added = true;
+                    break;
                 }
             }
+        }
+
+        if(!added){
+            newTag = new Tag(tag);
+            long tagId;
+            tagId = tagsHelper.insertTag(newTag);
+            newTag.setId(tagId);
+        }
+
+        return newTag;
+    }
+
+    private List<Tag> generateTagList(){
+        List<Tag> addedTags = new ArrayList<Tag>();
+
+        for(String tag : tags){
+            addedTags.add(insertTag(tag));
         }
 
         return addedTags;
     }
 
+    private Media makeMedia(String path, String type){
+        Media media = null;
+        String[] validTypes = {"sound", "word", "image"};
+
+        for(String t : validTypes){
+            if(t.equalsIgnoreCase(type)){
+                media = new Media(textLabel, path, publicPictogram, type, author);
+                break;
+            }
+        }
+
+        return media;
+    }
+
     private Media insertMedia(Media media){
         MediaHelper mediaHelper = databaseHelper.mediaHelper;
-        int mediaId;
+        long mediaId;
 
         mediaId = mediaHelper.insertMedia(media);
         media.setId(mediaId);
@@ -84,23 +103,27 @@ public class StoragePictogram {
     }
 
     private Media generateMedia(){
-        //public Media(String name, String mPath, boolean mPublic, String mType, long ownerId) {
-        Media pictureMedia = new Media(textLabel, imagePath, publicPictogram, "picture", author);
-                MediaHelper mediaHelper = databaseHelper.mediaHelper;
+        Media pictureMedia =  makeMedia(imagePath, "image");
+        MediaHelper mediaHelper = databaseHelper.mediaHelper;
 
         pictureMedia = insertMedia(pictureMedia);
-        mediaHelper.insertMedia(pictureMedia);
 
         if(!audioPath.equals("") && audioPath != null){
-            // this part is fucking stupid:
-            Media audioMedia = new Media(textLabel, audioPath, publicPictogram, "sound", author);
-            mediaHelper.insertMedia(audioMedia);
+            // this part is pretty dumb:
+            Media audioMedia = makeMedia(audioPath, "sound");
+            audioMedia = insertMedia(audioMedia);
+
+            mediaHelper.attachSubMediaToMedia(audioMedia, pictureMedia);
         }
 
         return pictureMedia;
     }
 
-    public Boolean addPictogram(){
-        return null;
+    public boolean addPictogram(){
+        Media media;
+
+        media = generateMedia();
+        if(media != null){}
+        return true;
     }
 }
