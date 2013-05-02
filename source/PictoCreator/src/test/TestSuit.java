fb8779d00274118d72f463b797a0262204851221
Mark Faldborg <mark@faldb.org>
2013-05-02 14:50:13 +0200

    StoragePictogram committed, this is cloes to working.

---
+++ source/PictoCreator/src/test/TestSuit.java
@@ -0,0 +1,16 @@
+package test;
+
+import dk.aau.cs.giraf.pictocreator.*;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author: Croc
+ */
+@RunWith(Suite.class)
+@SuiteClasses({StoragePictogram.class})
+public class TestSuit{
+
+}
