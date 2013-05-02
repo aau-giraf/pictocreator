9d01b99f99d4d1f8fcc864608d65d0ec100c7fdf
René K. Hornbjerg <r.hornbjerg@gmail.com>
2013-05-02 20:06:27 +0200

    Save dialog changes - Looks nice now

--- source/PictoCreator/src/dk/aau/cs/giraf/pictocreator/audiorecorder/RecordDialogFragment.java
+++ source/PictoCreator/src/dk/aau/cs/giraf/pictocreator/audiorecorder/RecordDialogFragment.java
@@ -51,18 +51,19 @@ public class RecordDialogFragment extends DialogFragment implements RecordInterf
 
     DecibelMeterView decibelMeter;
 
-    RecordThread recThread;
+    private RecordThread recThread;
 
-    ToggleButton recordButton;
+    private ToggleButton recordButton;
 
-    ImageButton acceptButton;
+    private ImageButton acceptButton;
 
-    ImageButton cancelButton;
+    private ImageButton cancelButton;
+
 
     /**
      * Constructor for the Dialog
      * Left empty on purpose
-     */
+n     */
     public RecordDialogFragment() {
 
     }
@@ -119,9 +120,9 @@ public class RecordDialogFragment extends DialogFragment implements RecordInterf
 
         decibelMeter = (DecibelMeterView) view.findViewById(R.id.decibel_meter);
 
-        acceptButton = (ImageButton) view.findViewById(R.id.positive_dialog_button);
+        acceptButton = (ImageButton) view.findViewById(R.id.record_positive_button);
 
-        cancelButton = (ImageButton) view.findViewById(R.id.negative_dialog_button);
+        cancelButton = (ImageButton) view.findViewById(R.id.record_negative_button);
 
         OnClickListener clickListener = new OnClickListener() {
                 public void onClick(View view) {
