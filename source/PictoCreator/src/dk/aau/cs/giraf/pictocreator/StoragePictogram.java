65d3de07b364b9d402154455529de7f731c08aae
Mark Faldborg <mark@faldb.org>
2013-05-08 15:25:46 +0200

    Made a method for accessing the PictogramID field.

--- source/PictoCreator/src/dk/aau/cs/giraf/pictocreator/StoragePictogram.java
+++ source/PictoCreator/src/dk/aau/cs/giraf/pictocreator/StoragePictogram.java
@@ -58,6 +58,10 @@ public class StoragePictogram {
         this.author = author;
     }
 
+    public long getId(){
+        return pictogramID;
+    }
+
     public String getImagePath(){
         return imagePath;
     }
