d2ebaf8170eda8ac017f4feecbcd66c096d49298
René K. Hornbjerg <r.hornbjerg@gmail.com>
2013-05-14 15:10:29 +0200

    Changed the saving path of the canvas bitmap, and added function to call when save dialog is opened

--- source/PictoCreator/src/dk/aau/cs/giraf/pictocreator/canvas/DrawFragment.java
+++ source/PictoCreator/src/dk/aau/cs/giraf/pictocreator/canvas/DrawFragment.java
@@ -1,6 +1,7 @@
 package dk.aau.cs.giraf.pictocreator.canvas;
 
 import java.util.Currency;
+import java.io.FileNotFoundException;
 
 import dk.aau.cs.giraf.pictocreator.R;
 import dk.aau.cs.giraf.pictocreator.canvas.entity.BitmapEntity;
@@ -14,6 +15,7 @@ import dk.aau.cs.giraf.pictocreator.management.CamImportDialogFragment.ImportRes
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,48 +35,48 @@ public class DrawFragment extends Fragment {
 
 	public View view;
 	public DrawView drawView;
-	
+
 	ImageButton rectHandlerButton;
 	ImageButton ovalHandlerButton;
 	ImageButton lineHandlerButton;
 	ImageButton selectHandlerButton;
 	ImageButton freehandHandlerButton;
 	ImageButton importFragmentButton;
-	
+
 	CamImportDialogFragment importDialog;
-	
+
 	/**
 	 * Displays previews of current color choices.
 	 */
 	PreviewButton previewButton;
-	
+
 	/**
 	 * Quick reference to one of the other handler buttons, whichever is active.
 	 */
 	ImageButton activeHandlerButton;
-	
+
 	LinearLayout colorButtonToolbox;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		if (savedInstanceState != null) {
 			// Restore drawStack et al.
 			drawView.drawStack = savedInstanceState.getParcelable("drawstack");
 		}
-		
+
 		Log.w("MainActivity", "Invalidating DrawView to force onDraw.");
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		view = inflater.inflate(R.layout.draw_fragment, container, false);
-		
+
 		drawView = (DrawView)view.findViewById(R.id.drawingview);
-		
+
 		freehandHandlerButton = (ImageButton)view.findViewById(R.id.freehand_handler_button);
 		freehandHandlerButton.setOnClickListener(onFreehandHandlerButtonClick);
 		selectHandlerButton = (ImageButton)view.findViewById(R.id.select_handler_button);
@@ -85,15 +87,15 @@ public class DrawFragment extends Fragment {
 		lineHandlerButton.setOnClickListener(onLineHandlerButtonClick);
 		ovalHandlerButton = (ImageButton)view.findViewById(R.id.oval_handler_button);
 		ovalHandlerButton.setOnClickListener(onOvalHandlerButtonClick);
-		
+
 		SeekBar strokeWidthBar = (SeekBar)view.findViewById(R.id.strokeWidthBar);
 		strokeWidthBar.setOnSeekBarChangeListener(onStrokeWidthChange);
-		
+
 		// Set initial handler.
 		drawView.setHandler(new FreehandHandler());
-		activeHandlerButton = freehandHandlerButton; 
+		activeHandlerButton = freehandHandlerButton;
 		activeHandlerButton.setEnabled(false);
-		
+
 		View tmp = view.findViewById(R.id.canvasColorPreviewButton);
 		Log.w("DrawFragment.onCreateView", String.format("PreviewButton returned as a %s.", tmp.getClass().toString()));
 		previewButton = (PreviewButton)tmp;
@@ -101,12 +103,12 @@ public class DrawFragment extends Fragment {
 		previewButton.setFillColor(0xFFFFFFFF);
 		drawView.setStrokeColor(previewButton.getStrokeColor());
 		drawView.setFillColor(previewButton.getFillColor());
-		
+
 		importFragmentButton = (ImageButton)view.findViewById(R.id.start_import_dialog_button);
 		importFragmentButton.setOnClickListener(onImportClick);
-		
+
 		colorButtonToolbox = (LinearLayout)((ScrollView)view.findViewById(R.id.colorToolbox)).getChildAt(0);
-		
+
 		// Add standard HTML colors to the box.
 		addColorButton(0x00000000); // Transparent
 		addColorButton(0xFF000000); // Black
@@ -125,14 +127,14 @@ public class DrawFragment extends Fragment {
 		addColorButton(0xFF0000FF); // Blue
 		addColorButton(0xFF800080); // Purple
 		addColorButton(0xFFFF00FF); // Fuchsia
-		
+
 		colorButtonToolbox.removeViewAt(0); // Remove the placeholder.
-		
+
 		strokeWidthBar.setProgress(4); // Set default stroke width.
-		
+
 		return view;
 	}
-	
+
 	/**
 	 * Adds a new ColorButton with the specified hex color to the color toolbox.
 	 * @param color Color to add. Can have alpha, although we suggest keeping it opaque (0xFFxxxxxx).
@@ -146,7 +148,19 @@ public class DrawFragment extends Fragment {
 						color,
 						this.getActivity()));
 	}
-	
+
+
+    public Bitmap saveBitmap(){
+        Bitmap tmp = null;
+        try {
+            tmp = drawView.saveToBitmap(Bitmap.Config.ARGB_8888);
+        }
+        catch (FileNotFoundException e){
+            Log.e(TAG, "No file was found to decode");
+        }
+        return tmp;
+    }
+
 	/**
 	 * Shorthand for switching active handler button. Better than serious code redundancy.
 	 * @param button The ImageButton to disable. The currently disabled button will be re-enabled.
@@ -156,65 +170,65 @@ public class DrawFragment extends Fragment {
 		activeHandlerButton = button; // Switch out active.
 		activeHandlerButton.setEnabled(false); // Disable new active.
 	}
-	
+
 	private final OnClickListener onSelectHandlerButtonClick = new OnClickListener() {
-		
+
 		@Override
 		public void onClick(View view) {
 			drawView.setHandler(new SelectionHandler(getResources()));
 			setActiveButton(selectHandlerButton);
 		}
 	};
-	
+
 	private final OnClickListener onFreehandHandlerButtonClick = new OnClickListener() {
-		
+
 		@Override
 		public void onClick(View view) {
 			drawView.setHandler(new FreehandHandler());
 			setActiveButton(freehandHandlerButton);
 		}
 	};
-	
+
 	/**
 	 * Click handler for the RectHandler. It sets the active handler in
 	 * DrawView and marks itself as "in use".
 	 */
 	private final OnClickListener onRectHandlerButtonClick = new OnClickListener() {
-		
+
 		@Override
 		public void onClick(View view) {
 			drawView.setHandler(new RectHandler());
 			setActiveButton(rectHandlerButton);
 		}
 	};
-	
+
 	private final OnClickListener onOvalHandlerButtonClick = new OnClickListener() {
-		
+
 		@Override
 		public void onClick(View view) {
 			drawView.setHandler(new OvalHandler());
 			setActiveButton(ovalHandlerButton);
 		}
 	};
-	
+
 	private final OnClickListener onLineHandlerButtonClick = new OnClickListener() {
-		
+
 		@Override
 		public void onClick(View view) {
 			drawView.setHandler(new LineHandler());
 			setActiveButton(lineHandlerButton);
 		}
 	};
-	
+
 	public void onSaveInstanceState(Bundle outState) {
 		// Parcel currentDrawStack = Parcel.obtain();
 		// drawView.drawStack.writeToParcel(currentDrawStack, 0);
 		Log.i("DrawFragment.onSaveInstanceState", "Saving drawstack in Bundled parcel.");
 		outState.putParcelable("drawstack", drawView.drawStack);
 	};
-	
+
 	private final OnClickListener onImportClick = new OnClickListener() {
-		
+
 		@Override
 		public void onClick(View view) {
 			importDialog = new CamImportDialogFragment();
@@ -230,14 +244,14 @@ public class DrawFragment extends Fragment {
 			importDialog.show(getActivity().getFragmentManager(), TAG);
 		}
 	};
-	
+
 	private final OnSeekBarChangeListener onStrokeWidthChange = new OnSeekBarChangeListener() {
 		public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 			previewButton.setStrokeWidth(progress);
 			drawView.setStrokeWidth(progress);
 			Log.i("DrawFragment", String.format("StrokeWidthBar changed to %s.", progress));
 		};
-		
+
 		public void onStopTrackingTouch(SeekBar seekBar) {};
 		public void onStartTrackingTouch(SeekBar seekBar) {};
 	};
--- source/PictoCreator/src/dk/aau/cs/giraf/pictocreator/canvas/DrawView.java
+++ source/PictoCreator/src/dk/aau/cs/giraf/pictocreator/canvas/DrawView.java
@@ -3,6 +3,8 @@ package dk.aau.cs.giraf.pictocreator.canvas;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import dk.aau.cs.giraf.pictocreator.canvas.entity.BitmapEntity;
 
@@ -20,43 +22,49 @@ import android.view.View;
 
 public class DrawView extends View {
 
+    private Context context;
+
 	protected int width, height;
-	
+
 	/**
 	 * This stack contains all current layers in the *drawing*. See drawStack
 	 * for the entire rendering stack used.
 	 */
 	EntityGroup drawStack = new EntityGroup();
-	
+
 	/**
 	 * The currently active handler for touch events.
 	 */
 	protected ActionHandler currentHandler;
-	
+
 	/**
 	 * Bounds for the image. These bounds will be used when saving to a bitmap,
 	 * while the entire drawStack is saved when stored as XML.
 	 */
 	Rect imageBounds;
-	
+
 	Paint boundsPaint = new Paint();
-	
+
 	private int strokeColor = 0xFF000000;
 	private int fillColor = 0xCCFF0000;
 	private float strokeWidth = 4;
-	
+
 	public DrawView(Context context) {
 		super(context);
+                this.context = context;
 		initStuff();
 	}
 
 	public DrawView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+                this.context = context;
+
 		initStuff();
 	}
 
 	public DrawView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
+                this.context = context;
 		initStuff();
 	}
 
@@ -66,30 +74,30 @@ public class DrawView extends View {
 		boundsPaint.setStrokeWidth(5);
 		boundsPaint.setStyle(Style.FILL);
 	}
-	
+
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		this.width = w;
 		this.height = h;
-		
+
 		resetImageBounds();
-		
+
 		super.onSizeChanged(w, h, oldw, oldh);
 	}
-	
+
 	public void resetImageBounds() {
 		int padding = 20; // Random padding.
 		// Calc middle.
 		int middle = this.width/2;
 		// Calc height of rect.
-		int rHeight = this.height - padding; 
+		int rHeight = this.height - padding;
 		imageBounds = new Rect(
 				middle-(rHeight/2),
 				padding,
 				middle+(rHeight/2),
 				rHeight-(padding/2));
 	}
-	
+
 	/**
 	 * Sets the active handler.
 	 * @param handler The handler to use.
@@ -100,54 +108,54 @@ public class DrawView extends View {
 		this.currentHandler.setStrokeColor(strokeColor);
 		this.currentHandler.setStrokeWidth(strokeWidth);
 	}
-	
+
 	public ActionHandler getCurrentHandler() {
 		return currentHandler;
 	}
-	
+
 	public void setFillColor(int color) {
 		this.currentHandler.setFillColor(color);
 		this.fillColor = color;
 		invalidate(); // Handler may need redraw.
 	}
-	
+
 	public void setStrokeColor(int color) {
 		this.currentHandler.setStrokeColor(color);
 		this.strokeColor = color;
 		invalidate(); // Handler may need redraw.
 	}
-	
+
 	public void setStrokeWidth(float width) {
 		this.currentHandler.setStrokeWidth(width);
 		this.strokeWidth = width;
 		invalidate();
 	}
-	
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		Log.w("DrawView",  "Invalidated. Redrawing.");
-		
+
 		// Drawing order: drawStack, drawBuffer, bounds.
 		drawStack.draw(canvas);
-		
+
 		if (currentHandler != null) currentHandler.drawBufferPreBounds(canvas);
-		
+
 		drawBounds(canvas);
-		
+
 		if (currentHandler != null) currentHandler.drawBufferPostBounds(canvas);
 	}
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		// Always draw for now. Very inefficient, but the optimisation is too demanding for now.
-		// We need some way of messaging a "dirty" state of parts of the draw stack. 
+		// We need some way of messaging a "dirty" state of parts of the draw stack.
 		invalidate();
-		
+
 		if (currentHandler.onTouchEvent(event, drawStack)) {
 			return true;
 		} else return false;
 	}
-	
+
 	/**
 	 * Draw the bounds rectangle on a canvas. This is an inverted rectangle.
 	 * The inner parts of the rectangle are transparent, while the surrounding
@@ -165,30 +173,36 @@ public class DrawView extends View {
 		canvas.drawRect(right, boundsPaint);
 		canvas.drawRect(below, boundsPaint);
 	}
-	
+
 	/**
 	 * Flattens the drawStack and returns it as a Bitmap.
 	 * @return A Bitmap representation of the drawStack within the imageBounds.
-	 * @throws FileNotFoundException 
+	 * @throws FileNotFoundException
 	 */
 	public Bitmap saveToBitmap(Bitmap.Config config) throws FileNotFoundException {
 		Bitmap toRet = Bitmap.createBitmap(this.width, this.height, config);
-		
+
 		Canvas buff = new Canvas(toRet);
-		
+
 		// We use the existing draw method to create the buffer. Although it's more expensive in cycles and memory,
 		// I suspect the user doesn't mind the time spent when they are saving, anyway.
 		this.draw(buff);
-		
+
 		Bitmap doRet = Bitmap.createBitmap(toRet, imageBounds.left, imageBounds.top, imageBounds.width(), imageBounds.height());
-		
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yy-MM-dd-HHmmss");
+		String date = dateFormat.format(new Date());
+		String imageFile = "GImage_" + date + ".jpg";
+
 		doRet.compress(Bitmap.CompressFormat.JPEG, 100,
-				new FileOutputStream(new File(Environment.getExternalStoragePublicDirectory(
-						Environment.DIRECTORY_PICTURES), "crocimagetest.jpg")));
-		
+                               new FileOutputStream(new File(this.context.getCacheDir().getAbsolutePath(), "img/" + imageFile)));
+                                                             // Environment.getExternalStoragePublicDirectory(
+                                                             // Environment.DIRECTORY_PICTURES),
+
+
 		return doRet;
 	}
-	
+
 	/**
 	 * Add a new BitmapEntity from a Bitmap source instance.
 	 * @param src The source data.
