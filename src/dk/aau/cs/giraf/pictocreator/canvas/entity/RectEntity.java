a53f359f2e0981e02747427bd2c04d5a92f3c519
Daniel Fridjonsson <Dani0223@hotmail.com>
2014-03-10 13:52:47 +0100

    Collision detection with rotated rectangles is now accurate with the drawed entity.

--- src/dk/aau/cs/giraf/pictocreator/canvas/entity/RectEntity.java
+++ src/dk/aau/cs/giraf/pictocreator/canvas/entity/RectEntity.java
@@ -3,6 +3,9 @@ package dk.aau.cs.giraf.pictocreator.canvas.entity;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.drawable.shapes.RectShape;
+import android.util.Log;
+
+import dk.aau.cs.giraf.pictocreator.canvas.FloatPoint;
 
 /**
  * Simple Entity. It is basically a visible rect.
@@ -22,4 +25,35 @@ public class RectEntity extends PrimitiveEntity {
 
 		rs.draw(canvas, paint);
 	}
+
+    /**
+     * The four points are calculated using a rotation matrix, since they are different when the rectangle is rotated.
+     * The clicked point is then compared to the four sides, to check whether it is a right rotation of them.
+     * @param x The X-coordinate of the point to check.
+     * @param y The Y-coordinate of the point to check.
+     * @return
+     */
+    @Override
+    public boolean collidesWithPoint(float x, float y){
+        FloatPoint P = new FloatPoint(x,y);
+        FloatPoint A = rotationMatrix( -(getWidth()/2), -(getHeight()/2));
+        FloatPoint B = rotationMatrix((getWidth()/2), -(getHeight()/2));
+        FloatPoint C = rotationMatrix((getWidth()/2), (getHeight()/2));
+        FloatPoint D = rotationMatrix( -(getWidth()/2), (getHeight()/2));
+
+        return (rightOrientation(A, B, P) && rightOrientation(B, C, P) && rightOrientation(C, D, P) && rightOrientation(D, A, P));
+    }
+
+    /**
+     * This function is used to check whether a point is a right rotation of a side.
+     * If the clicked point is a right rotation of all the sides in the rectangle it means it is inside of it.
+     * This was used to improve the collision detection for rotated rectangles.
+     * @param a the start point of a vector
+     * @param b the endpoint of a vector
+     * @param p the clicked point
+     * @return
+     */
+    private Boolean rightOrientation(FloatPoint a, FloatPoint b, FloatPoint p){
+        return (b.y-a.y)*(p.x-b.x)<=(p.y-b.y)*(b.x-a.x);
+    }
 }
