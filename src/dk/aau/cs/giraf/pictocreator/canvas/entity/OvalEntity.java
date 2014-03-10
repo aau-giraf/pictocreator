0da1baf3ac848366d68ff8a4089e51bb8e1af2d8
Daniel Fridjonsson <Dani0223@hotmail.com>
2014-03-10 14:17:57 +0100

    Accurate ellipses collision detection implemented.

--- src/dk/aau/cs/giraf/pictocreator/canvas/entity/OvalEntity.java
+++ src/dk/aau/cs/giraf/pictocreator/canvas/entity/OvalEntity.java
@@ -21,4 +21,19 @@ public class OvalEntity extends PrimitiveEntity {
 		 
 		rs.draw(canvas, paint);
 	}
+
+    /**
+     * Gives an accurate collision detection with ellipses.
+     * Formula found from stackoverflow: http://stackoverflow.com/questions/7946187/point-and-ellipse-rotated-position-test-algorithm
+     * @param x The X-coordinate of the point to check.
+     * @param y The Y-coordinate of the point to check.
+     * @return
+     */
+    @Override
+    public boolean collidesWithPoint(float x, float y) {
+        float radAngle = (float)Math.toRadians(getAngle());
+        return ((Math.pow(Math.cos(radAngle)*(x-getCenter().x)+Math.sin(radAngle)*(y-getCenter().y),2)/Math.pow(getWidth()/2,2))+
+                (Math.pow(Math.sin(radAngle)*(x-getCenter().x)-Math.cos(radAngle)*(y-getCenter().y),2)/Math.pow(getHeight()/2,2))) <= 1;
+    }
+
 }
