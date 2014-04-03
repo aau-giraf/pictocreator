9d6b12b1cc96243ce78045a85c6694b988da7d1a
Søren Skibsted Als <s_s_a_92@hotmail.com>
2014-04-03 15:42:33 +0200

    Tags i dialogboksen virker nu, mangler save funktion dertil

--- src/dk/aau/cs/giraf/pictocreator/management/SaveDialogFragment.java
+++ src/dk/aau/cs/giraf/pictocreator/management/SaveDialogFragment.java
@@ -3,35 +3,39 @@ package dk.aau.cs.giraf.pictocreator.management;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
 
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import dk.aau.cs.giraf.gui.GButton;
+import dk.aau.cs.giraf.gui.GDialogMessage;
 import dk.aau.cs.giraf.pictocreator.MainActivity;
 import dk.aau.cs.giraf.pictocreator.R;
 import dk.aau.cs.giraf.pictocreator.StoragePictogram;
@@ -57,6 +61,7 @@ public class SaveDialogFragment extends DialogFragment{
 
     private EditText inputTextLabel;
     private EditText inlineTextLabel;
+    private EditText tagInputFind;
 
 
     private final String defaultTextLabel = "defaultTextLabel";
@@ -72,6 +77,8 @@ public class SaveDialogFragment extends DialogFragment{
     private FileHandler fileHandler;
     private boolean service = false;
 
+    private  ListView tagsListView;
+    private ArrayAdapter<String> tagArrayAdapter;
     /**
      * Constructor for the Dialog
      */
@@ -85,18 +92,6 @@ public class SaveDialogFragment extends DialogFragment{
      */
     public void setPreview(Bitmap preview){
         this.preview = preview;
-    }
-
-    /**
-     * Method for associating tags to the pictogram
-     * @param tags The list of tags to associate to the pictogram
-     */
-    public void setTags(Collection<String> tags){
-        //this.tags = (ArrayList<String>) tags;
-        this.tags = new ArrayList<String>();
-        for(int i = 0; i < 50; i++){
-            this.tags.add(Integer.toString(i));
-        }
     }
 
     /**
@@ -144,22 +139,22 @@ public class SaveDialogFragment extends DialogFragment{
 
         fileHandler = new FileHandler(parentActivity, storagePictogram);
 
-        ListView listView;
-        ArrayAdapter<String> arrayAdapter;
         parentActivity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-        if(tags == null){
-            setTags(this.tags);
-        }
 
-        arrayAdapter  = new ArrayAdapter<String>(parentActivity, R.layout.save_tag, tags);
+        this.tags = new ArrayList<String>();
+        tagArrayAdapter  = new ArrayAdapter<String>(parentActivity, R.layout.save_tag, tags);
 
         view = inflater.inflate(R.layout.save_dialog, container);
         previewView = (FrameLayout) view.findViewById(R.id.save_preview);
-        listView = (ListView) view.findViewById(R.id.save_tags_list);
-        listView.setAdapter(arrayAdapter);
+
 
         inputTextLabel = (EditText) view.findViewById(R.id.save_input_title);
         inlineTextLabel = (EditText) view.findViewById(R.id.edit_inline_text);
+
+        tagInputFind = (EditText) view.findViewById(R.id.save_input_find);
+        tagInputFind.setNextFocusDownId(R.id.save_input_find);
+        tagsListView = (ListView) view.findViewById(R.id.save_tags_list);
+        tagsListView.setAdapter(tagArrayAdapter);
 
         publicStatus = (CheckBox) view.findViewById(R.id.public_status);
 
@@ -217,47 +212,79 @@ public class SaveDialogFragment extends DialogFragment{
                 }
             });
 
-        acceptButton.setOnClickListener(new OnClickListener(){
+        acceptButton.setOnClickListener(new OnClickListener() {
 
-                @Override
-                public void onClick(View view){
-                    textLabel = inputTextLabel.getText().toString();
-                    inlineText = inlineTextLabel.getText().toString();
-                    Log.d(TAG, "TextLabel: " + textLabel);
+            @Override
+            public void onClick(View view) {
+                textLabel = inputTextLabel.getText().toString();
+                inlineText = inlineTextLabel.getText().toString();
+                Log.d(TAG, "TextLabel: " + textLabel);
 
-                    if(textLabel.matches("") || textLabel == null){
-                        Toast.makeText(parentActivity, "Venligst angiv et navn", Toast.LENGTH_SHORT).show();
-                        return;
+                if (textLabel.matches("") || textLabel == null) {
+                    Toast.makeText(parentActivity, "Venligst angiv et navn", Toast.LENGTH_SHORT).show();
+                    return;
                         /*SimpleDateFormat dateFormat = new SimpleDateFormat("yy-MM-dd-HHmmss");
                         String date = dateFormat.format(new Date());
                         String label = "Pictogram_" + date;
                         Log.d(TAG, "TextLavel was empty/null, replaced with " + label);
                         textLabel = label;*/
-                    }
-
-                    fileHandler.saveFinalFiles(textLabel, inlineText);
-
-                    storagePictogram.setpublicPictogram(publicStatus.isChecked() ? 1 : 0);
-
-                    if(storagePictogram.addPictogram()){
-                        Toast.makeText(parentActivity, "Pictogram gemt", Toast.LENGTH_SHORT).show();
-                    }
-
-                    if(service){
-                        Intent returnIntent = new Intent(MainActivity.getActionResult());
-                        returnIntent.putExtra("pictogramId", storagePictogram.getId());
-                        parentActivity.setResult(Activity.RESULT_OK, returnIntent);
-
-                        parentActivity.finish();
-                    }
-
-                    tmpDialog.dismiss();
                 }
-            });
 
+                fileHandler.saveFinalFiles(textLabel, inlineText);
+
+                storagePictogram.setpublicPictogram(publicStatus.isChecked() ? 1 : 0);
+
+                if (storagePictogram.addPictogram()) {
+                    Toast.makeText(parentActivity, "Pictogram gemt", Toast.LENGTH_SHORT).show();
+                }
+
+                if (service) {
+                    Intent returnIntent = new Intent(MainActivity.getActionResult());
+                    returnIntent.putExtra("pictogramId", storagePictogram.getId());
+                    parentActivity.setResult(Activity.RESULT_OK, returnIntent);
+
+                    parentActivity.finish();
+                }
+
+                tmpDialog.dismiss();
+            }
+        });
+        tagInputFind.setOnKeyListener(tagKeyListener);
+        tagsListView.setOnItemClickListener(tagViewListener);
         return view;
     }
 
+    private static int tempPos;
+    private final AdapterView.OnItemClickListener tagViewListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            tempPos = position;
+            GDialogMessage removeDialog = new GDialogMessage(parentActivity,"Slet Tag: " + tags.get(position),
+                    new OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            tags.remove(tempPos);
+                            tagArrayAdapter.notifyDataSetChanged();
+                        }
+                    });
+            removeDialog.show();
+        }
+    };
+
+    private final View.OnKeyListener tagKeyListener = new View.OnKeyListener() {
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            Log.i(TAG, "code: " + keyCode);
+            if(keyCode == 66 && !(tagInputFind.getText().toString()).matches("")){
+                if(!tags.contains(tagInputFind.getText().toString())){
+                    tags.add(0,tagInputFind.getText().toString());
+                }
+                tagInputFind.setText("");
+                tagInputFind.requestFocus();
+            }
+            return false;
+        }
+    };
     /**
      * Method called when the dialog is resumed
      */
