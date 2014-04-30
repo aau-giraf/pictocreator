3c789947e3c9efbc7e61b0bfa2ee6c8ffb0f8086
Daniel Fridjonsson <Dani0223@hotmail.com>
2014-04-30 10:55:47 +0200

    Freehand is now toggled when the application is opened on the first time.

--- src/dk/aau/cs/giraf/pictocreator/canvas/DrawFragment.java
+++ src/dk/aau/cs/giraf/pictocreator/canvas/DrawFragment.java
@@ -114,7 +114,7 @@ public class DrawFragment extends Fragment {
 
         freehandHandlerButton = (GToggleButton)view.findViewById(R.id.freehand_handler_button);
         freehandHandlerButton.setOnClickListener(onFreehandHandlerButtonClick);
-        freehandHandlerButton.setPressed(true);
+        freehandHandlerButton.setToggled(true);
 
         selectHandlerButton = (GToggleButton)view.findViewById(R.id.select_handler_button);
         selectHandlerButton.setOnClickListener(onSelectHandlerButtonClick);
